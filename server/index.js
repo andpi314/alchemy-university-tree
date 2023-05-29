76cff11b971cd12e40fa98a0167be1f924668069
Andrea Provino <andrea.provino.dev@gmail.com>
2023-05-29 12:49:18 +0200

    feat(proof): simple verification system

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,19 +8,30 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "e264941eeedaf8b31c42030847b729dde0b61301332e658934a2526d5b105dd9";
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+function validateInput(body) {
+  if (!body.name) throw new Error("Missing name in body");
+  if (!body.proof) throw new Error("Missing proof in body");
+}
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+app.post("/gift", (req, res) => {
+  try {
+    // grab the parameters from the front-end here
+    const body = req.body;
+
+    validateInput(body);
+    const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
+    if (isInTheList) {
+      res.send("You got a toy robot!");
+    } else {
+      res.send("You are not on the list :(");
+    }
+  } catch (e) {
+    res.status(400).send({
+      message: e.message,
+    });
   }
 });
 
